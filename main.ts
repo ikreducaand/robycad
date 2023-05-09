f149348e9823c3a6fa875350ec2e1e961029cf3a
ikreducaand <132067470+ikreducaand@users.noreply.github.com>
2023-05-09 17:08:54 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,6 @@ maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 55) {
         maqueen.motorStop(maqueen.Motors.All)
-        pins.servoWritePin(AnalogPin.P0, 100)
+        pins.servoWritePin(AnalogPin.P1, 100)
     }
 })
