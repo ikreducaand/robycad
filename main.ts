f978ba50f427d5350a12bcc67e657d3db0a79fe6
ikreducaand <132067470+ikreducaand@users.noreply.github.com>
2023-05-09 17:16:05 +0200

    Beta1

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 pins.servoSetPulse(AnalogPin.P1, 1500)
 pins.servoWritePin(AnalogPin.P1, 0)
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 55) {
         maqueen.motorStop(maqueen.Motors.All)
         pins.servoWritePin(AnalogPin.P1, 100)
+        basic.pause(2000)
+    } else {
+        basic.pause(5000)
+        pins.servoWritePin(AnalogPin.P1, 0)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     }
 })
