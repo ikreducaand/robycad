275ee52ace7a788b73547e2ce9cc72eb421c9cc8
ikreducaand <132067470+ikreducaand@users.noreply.github.com>
2023-05-09 17:06:39 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+pins.servoSetPulse(AnalogPin.P1, 1500)
+pins.servoWritePin(AnalogPin.P1, 0)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
 basic.forever(function () {
-	
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 55) {
+        maqueen.motorStop(maqueen.Motors.All)
+        pins.servoWritePin(AnalogPin.P0, 100)
+    }
 })
