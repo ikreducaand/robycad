4ca5d6fbf76e2290556576639a1ac533f9a089b2
ikreducaand <132067470+ikreducaand@users.noreply.github.com>
2023-05-11 11:30:28 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,25 @@
+let strip: neopixel.Strip = null
 pins.servoSetPulse(AnalogPin.P1, 1500)
-pins.servoWritePin(AnalogPin.P1, 0)
+pins.servoWritePin(AnalogPin.P1, 94)
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 55) {
         maqueen.motorStop(maqueen.Motors.All)
-        pins.servoWritePin(AnalogPin.P1, 100)
+        pins.servoWritePin(AnalogPin.P1, 0)
         basic.pause(2000)
     } else {
         basic.pause(5000)
-        pins.servoWritePin(AnalogPin.P1, 0)
+        pins.servoWritePin(AnalogPin.P1, 103)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     }
+})
+control.inBackground(function () {
+    strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+    strip.showRainbow(1, 360)
+    basic.pause(100)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(100)
+    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    basic.pause(100)
+    strip.showColor(neopixel.colors(NeoPixelColors.White))
+    basic.pause(100)
 })
